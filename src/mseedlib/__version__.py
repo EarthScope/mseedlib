c82e415a2e6ed04dac69d59d9fac17105d4aef31
chad-earthscope <chad.trabant@earthscope.org>
2024-01-14 15:02:49 -0800

    Use single-source version at VERSION

--- src/mseedlib/__version__.py
+++ src/mseedlib/__version__.py
@@ -1,15 +1,20 @@
 import os
 
+module_path = os.path.abspath(os.path.dirname(__file__))
+
 # Module version
-__version__ = '0.0.1'
+__version__ = None
+
+with open(os.path.join(module_path, 'VERSION')) as version_file:
+    __version__ = version_file.read().strip()
 
 
 def libmseed_version():
     """Return the version of libmseed used by this module"""
-    module_path = os.path.abspath(os.path.dirname(__file__))
     libh = os.path.join(module_path, 'libmseed', "libmseed.h")
 
     with open(libh, 'r') as f:
         for line in f:
             if line.startswith("#define LIBMSEED_VERSION"):
                 return line.split()[2].replace('"', '')
+
