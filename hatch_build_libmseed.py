1791de61120f66449b2d285704fa8c2f311dfeee
chad-earthscope <chad.trabant@earthscope.org>
2024-01-15 12:29:03 -0800

    Add clean method for hatch build hook for libmseed

--- hatch_build_libmseed.py
+++ hatch_build_libmseed.py
@@ -6,32 +6,53 @@ import subprocess
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class CustomBuildHook(BuildHookInterface):
-    def initialize(self, version, build_data):
-        here = os.path.abspath(os.path.dirname(__file__))
-        package_path = os.path.join(here, 'src', 'mseedlib')
-        libmseed_path = os.path.join(package_path, 'libmseed')
 
-        print(f"Building libmseed via Makefile in {libmseed_path}")
+    here = os.path.abspath(os.path.dirname(__file__))
+    package_path = os.path.join(here, 'src', 'mseedlib')
+    libmseed_path = os.path.join(package_path, 'libmseed')
+
+    def initialize(self, version, build_data):
+        print(f"Building libmseed via Makefile in {self.libmseed_path}")
 
         if sys.platform.lower().startswith("windows"):
-            cmd = f"nmake /f Makefile.win -C {libmseed_path} dll"
+            cmd = f"pushd {self.libmseed_path} && nmake /f Makefile.win dll & popd"
         else:
-            cmd = f"make -C {libmseed_path} -j shared"
+            cmd = f"make -C {self.libmseed_path} -j shared"
             env = {'CFLAGS': '-O3'}
 
         subprocess.check_call(cmd, env=env, shell=True)
 
         # Copy shared library to root package location
-        if os.path.exists(os.path.join(libmseed_path, 'libmseed.so')):
-            shutil.copy(os.path.join(libmseed_path, 'libmseed.so'),
-                        os.path.join(package_path))
-        elif os.path.exists(os.path.join(libmseed_path, 'libmseed.dylib')):
-            shutil.copy(os.path.join(libmseed_path, 'libmseed.dylib'),
-                        os.path.join(package_path))
-        elif os.path.exists(os.path.join(libmseed_path, 'libmseed.dll')):
-            shutil.copy(os.path.join(libmseed_path, 'libmseed.dll'),
-                        os.path.join(package_path))
+        if os.path.exists(os.path.join(self.libmseed_path, 'libmseed.so')):
+            shutil.copy(os.path.join(self.libmseed_path, 'libmseed.so'),
+                        os.path.join(self.package_path))
+        elif os.path.exists(os.path.join(self.libmseed_path, 'libmseed.dylib')):
+            shutil.copy(os.path.join(self.libmseed_path, 'libmseed.dylib'),
+                        os.path.join(self.package_path))
+        elif os.path.exists(os.path.join(self.libmseed_path, 'libmseed.dll')):
+            shutil.copy(os.path.join(self.libmseed_path, 'libmseed.dll'),
+                        os.path.join(self.package_path))
 
         # Copy libmseed.h to root package location
-        shutil.copy(os.path.join(libmseed_path, 'libmseed.h'),
-                    os.path.join(package_path))
+        shutil.copy(os.path.join(self.libmseed_path, 'libmseed.h'),
+                    os.path.join(self.package_path))
+
+    def clean(self, versions):
+        if sys.platform.lower().startswith("windows"):
+            cmd = f"pushd {self.libmseed_path} && nmake /f Makefile.win clean & popd"
+        else:
+            cmd = f"make -C {self.libmseed_path} clean"
+
+        subprocess.check_call(cmd, shell=True)
+
+        if os.path.exists(os.path.join(self.package_path, 'libmseed.so')):
+            os.remove(os.path.join(self.package_path, 'libmseed.so'))
+        if os.path.exists(os.path.join(self.package_path, 'libmseed.dylib')):
+            os.remove(os.path.join(self.package_path, 'libmseed.dylib'))
+        if os.path.exists(os.path.join(self.package_path, 'libmseed.dll')):
+            os.remove(os.path.join(self.package_path, 'libmseed.dll'))
+        if os.path.exists(os.path.join(self.package_path, 'libmseed.h')):
+            os.remove(os.path.join(self.package_path, 'libmseed.h'))
+
+    def finalize(self, version, build_data, artifact_path):
+        pass
